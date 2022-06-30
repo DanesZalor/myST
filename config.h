f528f71088dd03095ab6815a437c78381563eb72
Daniel James Dolor <estheryahe@gmail.com>
2022-06-30 12:01:32 +0800

    latest patch idk what i changed i alr forgot should be the same anyways

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Fira Mono:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 10;
 
 /*
